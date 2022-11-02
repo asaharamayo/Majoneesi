15bf203c6b8695a0a4894993fa3234349f54d7ed
asaharamayo <shiuyukkeiangel@gmail.com>
2022-11-03 04:00:47 +0800

    added dad features

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -9,14 +9,18 @@ class HiCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self,message):
-        x = message.content.casefold()
+        a = message.content.casefold()
+        dad = 'dad'
         Triggers = {
             'im ', 'i am ', "i'm "
         }
         
         for y in Triggers:
-            if y in x:
-                await message.channel.send('Hi ' + str(x.split(y)[1] + ' \N{SKULL}'))
+            if y in a and dad in a:
+                await message.channel.send('Hi daddy allowance <:die_you:1037445906729017365>')
+            elif y in a and dad not in a:
+                await message.channel.send('Hi ' + str(message.content.split(y)[1] + ' \N{SKULL}'))
+                break
         
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(HiCog(bot))
