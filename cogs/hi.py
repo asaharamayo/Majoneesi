e19bf9133f981a94b24c33afabe51faac92f2ca4
asaharamayo <shiuyukkeiangel@gmail.com>
2022-11-03 19:14:48 +0800

    updated to pattern matching, capitalisations

--- cogs/hi.py
+++ cogs/hi.py
@@ -8,19 +8,16 @@ class HiCog(commands.Cog):
         self.bot=bot
 
     @commands.Cog.listener()
-    async def on_message(self,message):
+    async def on_message(self,message: discord.Message, /) -> None:
+        pattern: re.Pattern[str] = re.compile(r"\b(?:im|i am|i\'m)\b\s(.*)")
         a = message.content.casefold()
         dad = 'dad'
-        Triggers = {
-            'im ', 'i am ', 'i\'m '
-        }
-        
-        for y in Triggers:
-            if y in a and dad in a:
-                await message.channel.send('Hi daddy allowance <:die_you:1037445906729017365>', reference=message)
-            elif y in a and dad not in a:
-                await message.channel.send('Hi ' + str(message.content.split(y)[1]) + ' \N{SKULL}', reference=message)
-                break
+        if match := pattern.search(a):
+            if dad in a:
+                await message.channel.send(f'Hi daddy allowance <:die_you:1037445906729017365>', reference=message)
+            else:
+                len_show = message.content[-len(match[1]):]
+                await message.channel.send(f"Hi {len_show} \N{SKULL}", reference=message)
         
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(HiCog(bot))
