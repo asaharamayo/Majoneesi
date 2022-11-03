cc6771bcd9b7ee145b3246fd1fb20dba543fbdbd
asaharamayo <shiuyukkeiangel@gmail.com>
2022-11-04 06:30:58 +0800

    added hi dad and 69 trigger

--- cogs/autoreply.py
+++ cogs/autoreply.py
@@ -9,7 +9,7 @@ class AutoCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self,message: discord.Message, /) -> None:
-        pattern: re.Pattern[str] = re.compile(r"\b(?:im|i am|i\'m)\b\s(.*)", flags=re.I)
+        pattern: re.Pattern[str] = re.compile(r"\b(?:im|i am|i\'m|I\’m)\b\s(.*)", flags=re.I)
         a = message.content.casefold()
         dad = 'dad'
         if match := pattern.search(message.content):
@@ -17,13 +17,20 @@ class AutoCog(commands.Cog):
                 await message.channel.send(f'Hi daddy allowance <:die_you:1037445906729017365>', reference=message)
             else:
                 await message.channel.send(f'Hi {match[1]} \N{SKULL}', reference=message)
-        ily: re.Pattern[str] = re.compile(r"\b(?:i love you|i luv you|ily|i luv u|i love u)\b\s(.*)", flags=re.I)
+        ily: re.Pattern[str] = re.compile(r"\b(?:love|luv|ily)\b\s(.*)", flags=re.I)
         mayo = 'mayo'
         if match := ily.search(message.content) and mayo in message.content.casefold():
             if message.author.bot:
                 return
             else:
                 await message.channel.send(f'I love you too <@!{message.author.id}> ♡ ', reference=message)
+        if '69' in message.content.casefold():
+            await message.add_reaction('\U0001f602')
+        if 'hi dad' in message.content.casefold():
+            if message.author.bot:
+                return
+            else:
+                await message.channel.send(f'Hi daddy allowance <:die_you:1037445906729017365>', reference=message)
         
             
 async def setup(bot: commands.Bot) -> None:
