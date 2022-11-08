55cfddfeb374561849b6a62076670e7fc7b9a64f
Mayo <asaharamayo@gmail.com>
2022-11-08 17:19:33 +0000

    try 1

--- cogs/image_lib.py
+++ cogs/image_lib.py
@@ -10,20 +10,21 @@ class image_lib(commands.Cog):
     
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.file = discord.File(f'{self.bot.config["image_lib"]["path"]}/{random.choice (os.listdir(self.bot.config["image_lib"]["path"]))}', filename = "image.png")
+        self.path = f'{os.getcwd()}{self.bot.config["image_lib"]["path"]}'
+        self.file = discord.File(f'{self.path}/{random.choice (os.listdir(self.path))}', filename = "image.png")
         
     async def image_rand(self):
-        list = os.listdir(self.bot.config["image_lib"]["path"])
+        list = os.listdir(self.path)
         image_str = random.choice(list)
-        path = f'{self.bot.config["image_lib"]["path"]}/' + image_str
-        self.file = discord.File(f'{path}', filename = "image.png")
+        path_i = f'{self.path}/' + image_str
+        self.file = discord.File(f'{path_i}', filename = "image.png")
 
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self,payload):
         if str(payload.emoji) == '\U0001fac0' and payload.user_id == self.bot.config["user_id"]["owner"]:
             message = await self.bot.get_channel(self.bot.config["image_lib"]["channel_id"]).fetch_message(payload.message_id)
-            await message.attachments[0].save(f'./cogs/image_lib/{payload.message_id}.png')
+            await message.attachments[0].save(f'{self.path}/{payload.message_id}.png')
             await self.bot.get_channel(self.bot.config["channel_id"]["log"]).send('Image has been saved to Image_Lib')
 
 
